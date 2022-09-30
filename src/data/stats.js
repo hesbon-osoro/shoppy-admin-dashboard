3ed5aaf035cc5d25cc2e0a57cccce5433311673f
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-30 15:39:15 +0300

    stats.js

---
+++ src/data/stats.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FiStar, FiShoppingCart } from 'react-icons/fi';
+import { BsChatLeft } from 'react-icons/bs';
+
+export const weeklyStats = [
+  {
+    icon: <FiShoppingCart />,
+    amount: '-$560',
+    title: 'Top Sales',
+    desc: 'Johnathan Doe',
+    iconBg: '#FB9678',
+    pcColor: 'red-600',
+  },
+  {
+    icon: <FiStar />,
+    amount: '-$560',
+    title: 'Best Seller',
+    desc: 'MaterialPro Admin',
+    iconBg: 'rgb(254, 201, 15)',
+    pcColor: 'red-600',
+  },
+  {
+    icon: <BsChatLeft />,
+    amount: '+$560',
+    title: 'Most Commented',
+    desc: 'Ample Admin',
+    iconBg: '#00C292',
+    pcColor: 'green-600',
+  },
+];
