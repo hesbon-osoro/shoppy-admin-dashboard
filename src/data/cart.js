17a6c25ac9e4c27de4e1c764b7466955e2a81b04
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-30 15:37:24 +0300

    cart.js

---
+++ src/data/cart.js
@@ -0,0 +1,24 @@
+import product5 from './images/product5.jpg';
+import product6 from './images/product6.jpg';
+import product7 from './images/product7.jpg';
+
+export const cartData = [
+  {
+    image: product5,
+    name: 'butterscotch ice-cream',
+    category: 'Milk product',
+    price: '$250',
+  },
+  {
+    image: product6,
+    name: 'Supreme fresh tomato',
+    category: 'Vegetable Item',
+    price: '$450',
+  },
+  {
+    image: product7,
+    name: 'Red color candy',
+    category: 'Food Item',
+    price: '$190',
+  },
+];
