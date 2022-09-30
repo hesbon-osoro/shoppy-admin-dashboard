b6c0b5e5bf737329186b4d2fb2632463f3473cbf
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-30 15:38:31 +0300

    medicalpro.js

---
+++ src/data/medicalpro.js
@@ -0,0 +1,48 @@
+import avatar from './images/avatar.jpg';
+import avatar2 from './images/avatar2.jpg';
+import avatar3 from './images/avatar3.png';
+import avatar4 from './images/avatar4.jpg';
+
+export const medicalproBranding = {
+  data: [
+    {
+      title: 'Due Date',
+      desc: 'Oct 23, 2021',
+    },
+    {
+      title: 'Budget',
+      desc: '$98,500',
+    },
+    {
+      title: 'Expense',
+      desc: '$63,000',
+    },
+  ],
+  teams: [
+    {
+      name: 'Bootstrap',
+      color: 'orange',
+    },
+    {
+      name: 'Angular',
+      color: '#FB9678',
+    },
+  ],
+  leaders: [
+    {
+      image: avatar2,
+    },
+    {
+      image: avatar3,
+    },
+    {
+      image: avatar2,
+    },
+    {
+      image: avatar4,
+    },
+    {
+      image: avatar,
+    },
+  ],
+};
