99967bd435f1bf7937c268d538b31a922d769344
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-30 15:37:31 +0300

    charts.js

---
+++ src/data/charts.js
@@ -0,0 +1,315 @@
+export const areaPrimaryXAxis = {
+  valueType: 'DateTime',
+  labelFormat: 'y',
+  majorGridLines: { width: 0 },
+  intervalType: 'Years',
+  edgeLabelPlacement: 'Shift',
+  labelStyle: { color: 'gray' },
+};
+
+export const areaPrimaryYAxis = {
+  labelFormat: '{value}%',
+  lineStyle: { width: 0 },
+  maximum: 4,
+  interval: 1,
+  majorTickLines: { width: 0 },
+  minorTickLines: { width: 0 },
+  labelStyle: { color: 'gray' },
+};
+export const barPrimaryXAxis = {
+  valueType: 'Category',
+  interval: 1,
+  majorGridLines: { width: 0 },
+};
+export const barPrimaryYAxis = {
+  majorGridLines: { width: 0 },
+  majorTickLines: { width: 0 },
+  lineStyle: { width: 0 },
+  labelStyle: { color: 'transparent' },
+};
+
+const areaChartData = [
+  [
+    { x: new Date(2002, 0, 1), y: 2.2 },
+    { x: new Date(2003, 0, 1), y: 3.4 },
+    { x: new Date(2004, 0, 1), y: 2.8 },
+    { x: new Date(2005, 0, 1), y: 1.6 },
+    { x: new Date(2006, 0, 1), y: 2.3 },
+    { x: new Date(2007, 0, 1), y: 2.5 },
+    { x: new Date(2008, 0, 1), y: 2.9 },
+    { x: new Date(2009, 0, 1), y: 3.8 },
+    { x: new Date(2010, 0, 1), y: 1.4 },
+    { x: new Date(2011, 0, 1), y: 3.1 },
+  ],
+  [
+    { x: new Date(2002, 0, 1), y: 2 },
+    { x: new Date(2003, 0, 1), y: 1.7 },
+    { x: new Date(2004, 0, 1), y: 1.8 },
+    { x: new Date(2005, 0, 1), y: 2.1 },
+    { x: new Date(2006, 0, 1), y: 2.3 },
+    { x: new Date(2007, 0, 1), y: 1.7 },
+    { x: new Date(2008, 0, 1), y: 1.5 },
+    { x: new Date(2009, 0, 1), y: 2.8 },
+    { x: new Date(2010, 0, 1), y: 1.5 },
+    { x: new Date(2011, 0, 1), y: 2.3 },
+  ],
+  [
+    { x: new Date(2002, 0, 1), y: 0.8 },
+    { x: new Date(2003, 0, 1), y: 1.3 },
+    { x: new Date(2004, 0, 1), y: 1.1 },
+    { x: new Date(2005, 0, 1), y: 1.6 },
+    { x: new Date(2006, 0, 1), y: 2 },
+    { x: new Date(2007, 0, 1), y: 1.7 },
+    { x: new Date(2008, 0, 1), y: 2.3 },
+    { x: new Date(2009, 0, 1), y: 2.7 },
+    { x: new Date(2010, 0, 1), y: 1.1 },
+    { x: new Date(2011, 0, 1), y: 2.3 },
+  ],
+];
+export const areaCustomSeries = [
+  {
+    dataSource: areaChartData[0],
+    xName: 'x',
+    yName: 'y',
+    name: 'USA',
+    opacity: '0.8',
+    type: 'SplineArea',
+    width: '2',
+  },
+  {
+    dataSource: areaChartData[1],
+    xName: 'x',
+    yName: 'y',
+    name: 'France',
+    opacity: '0.8',
+    type: 'SplineArea',
+    width: '2',
+  },
+  {
+    dataSource: areaChartData[2],
+    xName: 'x',
+    yName: 'y',
+    name: 'Germany',
+    opacity: '0.8',
+    type: 'SplineArea',
+    width: '2',
+  },
+];
+export const barChartData = [
+  [
+    { x: 'USA', y: 46 },
+    { x: 'GBR', y: 27 },
+    { x: 'CHN', y: 26 },
+  ],
+  [
+    { x: 'USA', y: 37 },
+    { x: 'GBR', y: 23 },
+    { x: 'CHN', y: 18 },
+  ],
+  [
+    { x: 'USA', y: 38 },
+    { x: 'GBR', y: 17 },
+    { x: 'CHN', y: 26 },
+  ],
+];
+export const barCustomSeries = [
+  {
+    dataSource: barChartData[0],
+    xName: 'x',
+    yName: 'y',
+    name: 'Gold',
+    type: 'Column',
+    marker: {
+      dataLabel: {
+        visible: true,
+        position: 'Top',
+        font: { fontWeight: '600', color: '#ffffff' },
+      },
+    },
+  },
+  {
+    dataSource: barChartData[1],
+    xName: 'x',
+    yName: 'y',
+    name: 'Silver',
+    type: 'Column',
+    marker: {
+      dataLabel: {
+        visible: true,
+        position: 'Top',
+        font: { fontWeight: '600', color: '#ffffff' },
+      },
+    },
+  },
+  {
+    dataSource: barChartData[2],
+    xName: 'x',
+    yName: 'y',
+    name: 'Bronze',
+    type: 'Column',
+    marker: {
+      dataLabel: {
+        visible: true,
+        position: 'Top',
+        font: { fontWeight: '600', color: '#ffffff' },
+      },
+    },
+  },
+];
+export const colorMappingData = [
+  [
+    { x: 'Jan', y: 6.96 },
+    { x: 'Feb', y: 8.9 },
+    { x: 'Mar', y: 12 },
+    { x: 'Apr', y: 17.5 },
+    { x: 'May', y: 22.1 },
+    { x: 'June', y: 25 },
+    { x: 'July', y: 29.4 },
+    { x: 'Aug', y: 29.6 },
+    { x: 'Sep', y: 25.8 },
+    { x: 'Oct', y: 21.1 },
+    { x: 'Nov', y: 15.5 },
+    { x: 'Dec', y: 9.9 },
+  ],
+  ['#FFFF99'],
+  ['#FFA500'],
+  ['#FF4040'],
+];
+
+export const rangeColorMapping = [
+  { label: '1°C to 10°C', start: '1', end: '10', colors: colorMappingData[1] },
+
+  {
+    label: '11°C to 20°C',
+    start: '11',
+    end: '20',
+    colors: colorMappingData[2],
+  },
+
+  {
+    label: '21°C to 30°C',
+    start: '21',
+    end: '30',
+    colors: colorMappingData[3],
+  },
+];
+
+export const ColorMappingPrimaryXAxis = {
+  valueType: 'Category',
+  majorGridLines: { width: 0 },
+  title: 'Months',
+};
+
+export const ColorMappingPrimaryYAxis = {
+  lineStyle: { width: 0 },
+  majorTickLines: { width: 0 },
+  minorTickLines: { width: 0 },
+  labelFormat: '{value}°C',
+  title: 'Temperature',
+};
+
+export const FinancialPrimaryXAxis = {
+  valueType: 'DateTime',
+  minimum: new Date('2016, 12, 31'),
+  maximum: new Date('2017, 9, 30'),
+  crosshairTooltip: { enable: true },
+  majorGridLines: { width: 0 },
+};
+
+export const FinancialPrimaryYAxis = {
+  title: 'Price',
+  minimum: 100,
+  maximum: 180,
+  interval: 20,
+  lineStyle: { width: 0 },
+  majorTickLines: { width: 0 },
+};
+
+export const LinePrimaryXAxis = {
+  valueType: 'DateTime',
+  labelFormat: 'y',
+  intervalType: 'Years',
+  edgeLabelPlacement: 'Shift',
+  majorGridLines: { width: 0 },
+  background: 'white',
+};
+
+export const LinePrimaryYAxis = {
+  labelFormat: '{value}%',
+  rangePadding: 'None',
+  minimum: 0,
+  maximum: 100,
+  interval: 20,
+  lineStyle: { width: 0 },
+  majorTickLines: { width: 0 },
+  minorTickLines: { width: 0 },
+};
+export const lineChartData = [
+  [
+    { x: new Date(2005, 0, 1), y: 21 },
+    { x: new Date(2006, 0, 1), y: 24 },
+    { x: new Date(2007, 0, 1), y: 36 },
+    { x: new Date(2008, 0, 1), y: 38 },
+    { x: new Date(2009, 0, 1), y: 54 },
+    { x: new Date(2010, 0, 1), y: 57 },
+    { x: new Date(2011, 0, 1), y: 70 },
+  ],
+  [
+    { x: new Date(2005, 0, 1), y: 28 },
+    { x: new Date(2006, 0, 1), y: 44 },
+    { x: new Date(2007, 0, 1), y: 48 },
+    { x: new Date(2008, 0, 1), y: 50 },
+    { x: new Date(2009, 0, 1), y: 66 },
+    { x: new Date(2010, 0, 1), y: 78 },
+    { x: new Date(2011, 0, 1), y: 84 },
+  ],
+
+  [
+    { x: new Date(2005, 0, 1), y: 10 },
+    { x: new Date(2006, 0, 1), y: 20 },
+    { x: new Date(2007, 0, 1), y: 30 },
+    { x: new Date(2008, 0, 1), y: 39 },
+    { x: new Date(2009, 0, 1), y: 50 },
+    { x: new Date(2010, 0, 1), y: 70 },
+    { x: new Date(2011, 0, 1), y: 100 },
+  ],
+];
+export const SparklineAreaData = [
+  { x: 1, yval: 2 },
+  { x: 2, yval: 6 },
+  { x: 3, yval: 8 },
+  { x: 4, yval: 5 },
+  { x: 5, yval: 10 },
+];
+
+export const lineCustomSeries = [
+  {
+    dataSource: lineChartData[0],
+    xName: 'x',
+    yName: 'y',
+    name: 'Germany',
+    width: '2',
+    marker: { visible: true, width: 10, height: 10 },
+    type: 'Line',
+  },
+
+  {
+    dataSource: lineChartData[1],
+    xName: 'x',
+    yName: 'y',
+    name: 'England',
+    width: '2',
+    marker: { visible: true, width: 10, height: 10 },
+    type: 'Line',
+  },
+
+  {
+    dataSource: lineChartData[2],
+    xName: 'x',
+    yName: 'y',
+    name: 'India',
+    width: '2',
+    marker: { visible: true, width: 10, height: 10 },
+    type: 'Line',
+  },
+];
