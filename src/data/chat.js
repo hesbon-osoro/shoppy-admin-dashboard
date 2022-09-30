eb375db6f70d0bf8201ea2ee67b4b7ebf4cd75b5
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-30 15:37:47 +0300

    chat.js

---
+++ src/data/chat.js
@@ -0,0 +1,31 @@
+import avatar from './images/avatar.jpg';
+import avatar2 from './images/avatar2.jpg';
+import avatar3 from './images/avatar3.png';
+import avatar4 from './images/avatar4.jpg';
+
+export const chatData = [
+  {
+    image: avatar2,
+    message: 'Roman Joined the Team!',
+    desc: 'Congratulate him',
+    time: '9:08 AM',
+  },
+  {
+    image: avatar3,
+    message: 'New message received',
+    desc: 'Salma sent you new message',
+    time: '11:56 AM',
+  },
+  {
+    image: avatar4,
+    message: 'New Payment received',
+    desc: 'Check your earnings',
+    time: '4:39 AM',
+  },
+  {
+    image: avatar,
+    message: 'Jolly completed tasks',
+    desc: 'Assign her new tasks',
+    time: '1:12 AM',
+  },
+];
