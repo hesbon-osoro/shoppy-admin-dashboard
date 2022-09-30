df55d20f2fbe20cdc177cb2de3f4a09dcd428633
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-30 15:39:40 +0300

    transactions.js

---
+++ src/data/transactions.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import { FiBarChart, FiCreditCard } from 'react-icons/fi';
+import { BsBoxSeam, BsCurrencyDollar, BsShield } from 'react-icons/bs';
+import { MdOutlineSupervisorAccount } from 'react-icons/md';
+import { HiOutlineRefresh } from 'react-icons/hi';
+import { TiTick } from 'react-icons/ti';
+
+export const earningData = [
+  {
+    icon: <MdOutlineSupervisorAccount />,
+    amount: '39,354',
+    percentage: '-4%',
+    title: 'Customers',
+    iconColor: '#03C9D7',
+    iconBg: '#E5FAFB',
+    pcColor: 'red-600',
+  },
+  {
+    icon: <BsBoxSeam />,
+    amount: '4,396',
+    percentage: '+23%',
+    title: 'Products',
+    iconColor: 'rgb(255, 244, 229)',
+    iconBg: 'rgb(254, 201, 15)',
+    pcColor: 'green-600',
+  },
+  {
+    icon: <FiBarChart />,
+    amount: '423,39',
+    percentage: '+38%',
+    title: 'Sales',
+    iconColor: 'rgb(228, 106, 118)',
+    iconBg: 'rgb(255, 244, 229)',
+
+    pcColor: 'green-600',
+  },
+  {
+    icon: <HiOutlineRefresh />,
+    amount: '39,354',
+    percentage: '-12%',
+    title: 'Refunds',
+    iconColor: 'rgb(0, 194, 146)',
+    iconBg: 'rgb(235, 250, 242)',
+    pcColor: 'red-600',
+  },
+];
+export const recentTransactions = [
+  {
+    icon: <BsCurrencyDollar />,
+    amount: '+$350',
+    title: 'Paypal Transfer',
+    desc: 'Money Added',
+    iconColor: '#03C9D7',
+    iconBg: '#E5FAFB',
+    pcColor: 'green-600',
+  },
+  {
+    icon: <BsShield />,
+    amount: '-$560',
+    desc: 'Bill Payment',
+    title: 'Wallet',
+    iconColor: 'rgb(0, 194, 146)',
+    iconBg: 'rgb(235, 250, 242)',
+    pcColor: 'red-600',
+  },
+  {
+    icon: <FiCreditCard />,
+    amount: '+$350',
+    title: 'Credit Card',
+    desc: 'Money reversed',
+    iconColor: 'rgb(255, 244, 229)',
+    iconBg: 'rgb(254, 201, 15)',
+
+    pcColor: 'green-600',
+  },
+  {
+    icon: <TiTick />,
+    amount: '+$350',
+    title: 'Bank Transfer',
+    desc: 'Money Added',
+
+    iconColor: 'rgb(228, 106, 118)',
+    iconBg: 'rgb(255, 244, 229)',
+    pcColor: 'green-600',
+  },
+  {
+    icon: <BsCurrencyDollar />,
+    amount: '-$50',
+    percentage: '+38%',
+    title: 'Refund',
+    desc: 'Payment Sent',
+    iconColor: '#03C9D7',
+    iconBg: '#E5FAFB',
+    pcColor: 'red-600',
+  },
+];
