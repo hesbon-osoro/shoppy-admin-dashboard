7ffa6dee11a811d5b21acaf508f3258154cb576a
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-30 15:39:00 +0300

    pyramid.js

---
+++ src/data/pyramid.js
@@ -0,0 +1,8 @@
+export const PyramidData = [
+  { x: 'Sweet Treats', y: 120, text: '120 cal' },
+  { x: 'Milk, Youghnut, Cheese', y: 435, text: '435 cal' },
+  { x: 'Vegetables', y: 470, text: '470 cal' },
+  { x: 'Meat, Poultry, Fish', y: 475, text: '475 cal' },
+  { x: 'Fruits', y: 520, text: '520 cal' },
+  { x: 'Bread, Rice, Pasta', y: 930, text: '930 cal' },
+];
