e552e6189ac563a35691c8ff86ff44fb857c29f5
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-30 15:37:41 +0300

    contextMenu.js

---
+++ src/data/contextMenu.js
@@ -0,0 +1,18 @@
+export const contextMenuItems = [
+  'AutoFit',
+  'AutoFitAll',
+  'SortAscending',
+  'SortDescending',
+  'Copy',
+  'Edit',
+  'Delete',
+  'Save',
+  'Cancel',
+  'PdfExport',
+  'ExcelExport',
+  'CsvExport',
+  'FirstPage',
+  'PrevPage',
+  'LastPage',
+  'NextPage',
+];
