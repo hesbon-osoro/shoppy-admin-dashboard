0b7be109a3d0664343732022d6217e1d64e0e2e7
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-30 15:38:55 +0300

    products.js

---
+++ src/data/products.js
@@ -0,0 +1,39 @@
+import product4 from './images/product4.jpg';
+import product5 from './images/product5.jpg';
+import product6 from './images/product6.jpg';
+import product7 from './images/product7.jpg';
+
+export const productsPerformance = [
+  {
+    image: product5,
+    title: 'Is it good butterscotch ice-cream?',
+    desc: 'Ice-Cream, Milk, Powder',
+    rating: 'Good',
+    itemSold: '65%',
+    earningAmount: '$546,000',
+  },
+  {
+    image: product6,
+    title: 'Supreme fresh tomato available',
+    desc: 'Market, Mall',
+    rating: 'Excellent',
+    itemSold: '98%',
+    earningAmount: '$780,000',
+  },
+  {
+    image: product7,
+    title: 'Red color candy from Gucci',
+    desc: 'Chocolate, Yummy',
+    rating: 'Average',
+    itemSold: '46%',
+    earningAmount: '$457,000',
+  },
+  {
+    image: product4,
+    title: 'Stylish night lamp for night',
+    desc: 'Electric, Wire, Current',
+    rating: 'Poor',
+    itemSold: '23%',
+    earningAmount: '$123,000',
+  },
+];
