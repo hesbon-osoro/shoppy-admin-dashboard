989f546d334a09ec480ed38c1e4c10eaa941cdd9
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-30 15:37:59 +0300

    ecomPieChart.js

---
+++ src/data/ecomPieChart.js
@@ -0,0 +1,6 @@
+export const ecomPieChartData = [
+  { x: '2018', y: 18, text: '35%' },
+  { x: '2019', y: 18, text: '15%' },
+  { x: '2020', y: 18, text: '25%' },
+  { x: '2021', y: 18, text: '25%' },
+];
