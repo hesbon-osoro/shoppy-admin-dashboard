acef364854aee6b7d3525f656eca39c250f73e05
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-30 15:37:52 +0300

    dropdown.js

---
+++ src/data/dropdown.js
@@ -0,0 +1,14 @@
+export const dropdownData = [
+  {
+    Id: '1',
+    Time: 'March 2021',
+  },
+  {
+    Id: '2',
+    Time: 'April 2021',
+  },
+  {
+    Id: '3',
+    Time: 'May 2021',
+  },
+];
