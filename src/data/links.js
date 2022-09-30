3c2eb58cedc3728dfe066a2b426a19a9cd2274aa
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-30 15:38:26 +0300

    links.js

---
+++ src/data/links.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import {
+  AiOutlineCalendar,
+  AiOutlineShoppingCart,
+  AiOutlineAreaChart,
+  AiOutlineBarChart,
+  AiOutlineStock,
+} from 'react-icons/ai';
+import { FiShoppingBag, FiEdit, FiPieChart } from 'react-icons/fi';
+import { BsKanban, BsBarChart } from 'react-icons/bs';
+import { BiColorFill } from 'react-icons/bi';
+import { IoMdContacts } from 'react-icons/io';
+import { RiContactsLine, RiStockLine } from 'react-icons/ri';
+import { GiLouvrePyramid } from 'react-icons/gi';
+
+export const links = [
+  {
+    title: 'Dashboard',
+    links: [
+      {
+        name: 'ecommerce',
+        icon: <FiShoppingBag />,
+      },
+    ],
+  },
+  {
+    title: 'Pages',
+    links: [
+      {
+        name: 'orders',
+        icon: <AiOutlineShoppingCart />,
+      },
+      {
+        name: 'employees',
+        icon: <IoMdContacts />,
+      },
+      {
+        name: 'customers',
+        icon: <RiContactsLine />,
+      },
+    ],
+  },
+  {
+    title: 'Apps',
+    links: [
+      {
+        name: 'calendar',
+        icon: <AiOutlineCalendar />,
+      },
+      {
+        name: 'kanban',
+        icon: <BsKanban />,
+      },
+      {
+        name: 'editor',
+        icon: <FiEdit />,
+      },
+      {
+        name: 'color-picker',
+        icon: <BiColorFill />,
+      },
+    ],
+  },
+  {
+    title: 'Charts',
+    links: [
+      {
+        name: 'line',
+        icon: <AiOutlineStock />,
+      },
+      {
+        name: 'area',
+        icon: <AiOutlineAreaChart />,
+      },
+
+      {
+        name: 'bar',
+        icon: <AiOutlineBarChart />,
+      },
+      {
+        name: 'pie',
+        icon: <FiPieChart />,
+      },
+      {
+        name: 'financial',
+        icon: <RiStockLine />,
+      },
+      {
+        name: 'color-mapping',
+        icon: <BsBarChart />,
+      },
+      {
+        name: 'pyramid',
+        icon: <GiLouvrePyramid />,
+      },
+      {
+        name: 'stacked',
+        icon: <AiOutlineBarChart />,
+      },
+    ],
+  },
+];
