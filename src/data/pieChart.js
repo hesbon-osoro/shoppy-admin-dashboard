7ec12aa8cc960da1082ae62c352c47ec2640d2bb
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-30 15:38:49 +0300

    pieChart.js

---
+++ src/data/pieChart.js
@@ -0,0 +1,9 @@
+export const pieChartData = [
+  { x: 'Labour', y: 18, text: '18%' },
+  { x: 'Legal', y: 8, text: '8%' },
+  { x: 'Production', y: 15, text: '15%' },
+  { x: 'License', y: 11, text: '11%' },
+  { x: 'Facilities', y: 18, text: '18%' },
+  { x: 'Taxes', y: 14, text: '14%' },
+  { x: 'Insurance', y: 16, text: '16%' },
+];
