5735e867b408aa8b7b13180ddc1246ff3cc63c39
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-30 15:39:33 +0300

    user.js

---
+++ src/data/user.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FiCreditCard } from 'react-icons/fi';
+import { BsCurrencyDollar, BsShield } from 'react-icons/bs';
+
+export const userProfileData = [
+  {
+    icon: <BsCurrencyDollar />,
+    title: 'My Profile',
+    desc: 'Account Settings',
+    iconColor: '#03C9D7',
+    iconBg: '#E5FAFB',
+  },
+  {
+    icon: <BsShield />,
+    title: 'My Inbox',
+    desc: 'Messages & Emails',
+    iconColor: 'rgb(0, 194, 146)',
+    iconBg: 'rgb(235, 250, 242)',
+  },
+  {
+    icon: <FiCreditCard />,
+    title: 'My Tasks',
+    desc: 'To-do and Daily Tasks',
+    iconColor: 'rgb(255, 244, 229)',
+    iconBg: 'rgb(254, 201, 15)',
+  },
+];
