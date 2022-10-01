d21a4a628ee224131604be36f05ed8a876351fd2
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-10-01 09:43:02 +0300

    index.jsx

---
+++ src/components/index.jsx
@@ -0,0 +1,15 @@
+export { default as Button } from './Button';
+export { default as ThemeSettings } from './ThemeSettings';
+export { default as Sidebar } from './Sidebar';
+export { default as Navbar } from './Navbar';
+export { default as Footer } from './Footer';
+export { default as Cart } from './Cart';
+export { default as Chat } from './Chat';
+export { default as Notification } from './Notification';
+export { default as UserProfile } from './UserProfile';
+export { default as Header } from './Header';
+export { default as ChartsHeader } from './ChartsHeader';
+export { default as SparkLine } from './charts/SparkLine';
+export { default as LineChart } from './charts/LineChart';
+export { default as Stacked } from './charts/Stacked';
+export { default as Pie } from './charts/Pie';
