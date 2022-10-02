9202e1a66e13f6d163266ec81b61c7d181ba7dce
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-10-02 23:21:00 +0300

    Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      &copy; {new Date().getFullYear()} All rights reserved by Shoppy.com
+    </p>
+  </div>
+);
+
+export default Footer;
