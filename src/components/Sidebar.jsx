8978d13ec67b8c52f11a86bfe4cfa9e5389905e6
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-10-02 23:22:29 +0300

    brackets

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -45,12 +45,12 @@ const Sidebar = () => {
             </TooltipComponent>
           </div>
           <div className="mt-10">
-            {links.map(item => (
+            {links.map((item) => (
               <div key={item.title}>
                 <p className="text-gray-400 dark:text-gray-400 m-3 mt-4 uppercase">
                   {item.name}
                 </p>
-                {item.links.map(link => (
+                {item.links.map((link) => (
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
