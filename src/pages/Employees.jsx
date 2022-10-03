43348d3e12380b198fbec3f775e6dc1cda826254
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-10-03 22:37:07 +0300

    Employees.jsx

---
+++ src/pages/Employees.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  ColumnDirective,
+  ColumnsDirective,
+  GridComponent,
+  Inject,
+  Page,
+  Search,
+} from '@syncfusion/ej2-react-grids';
+
+import { employeesData, employeesGrid } from '../data/employees';
+import { Header } from '../components';
+
+const Employees = () => {
+  const toolbarOptions = ['Search'];
+  const editing = { allowDeleting: true, allowEditing: true };
+
+  return (
+    <div className="m-2  md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        dataSource={employeesData}
+        width="auto"
+        allowPaging
+        allowSorting
+        pageSettings={{ pageCount: 5 }}
+        editSettings={editing}
+        toolbar={toolbarOptions}
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Search, Page]} />
+      </GridComponent>
+    </div>
+  );
+};
+
+export default Employees;
